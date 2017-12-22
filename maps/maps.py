fd4a5c24d56a2916d44212d2f9f21d308a3e8b95
kiko <kiko@Franciscos-MBP.fios-router.home>
2017-12-21 22:49:51 -0500

    workplan: added equations of CE model

---
+++ maps/maps.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import matplotlib.cm
+
+from mpl_toolkits.basemap import Basemap
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+from matplotlib.colors import Normalize
