f1633af405b8fa0cc5f6adb39cfd11d91fd93a3a
kiko <kiko@Franciscos-MBP.wv.cc.cmu.edu>
2018-01-02 13:06:58 -0500

    added example of python script to create map output

--- maps/maps.py
+++ maps/maps.py
@@ -1,7 +1,26 @@
 import matplotlib.pyplot as plt
-import matplotlib.cm
-
 from mpl_toolkits.basemap import Basemap
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-from matplotlib.colors import Normalize
+
+
+def draw_map_background(m, ax):
+    ax.set_facecolor('#729FCF')
+    m.fillcontinents(color='#FAFAFA', ax=ax, zorder=0, lake_color='#729FCF')
+    m.drawcounties(ax=ax, color='#c5c5c5')
+    m.drawrivers(ax=ax, color='#729FCF')
+    m.drawstates(ax=ax)
+    m.drawcountries(ax=ax)
+    m.drawcoastlines(ax=ax)
+
+
+ll_lat = 29.65
+ll_lon = -94.143
+ur_lat = 38.77
+ur_lon = -76.56
+m = Basemap(llcrnrlon=ll_lon, llcrnrlat=ll_lat, urcrnrlon=ur_lon, urcrnrlat=ur_lat,
+            resolution='i', area_thresh=2500.)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+draw_map_background(m, ax)
+
+plt.savefig('./maps/example.png', bbox_inches='tight')
+plt.close(fig)
