d0f7617ac3c5b291c6314b14ee60f8e214f68a4d
kikoralston <fralston@andrew.cmu.edu>
2018-11-21 18:01:46 -0500

    FIXED NAME OF LIST FILE THAT IS READ AFTER GAMS CALL

--- CE/RIPSMasterScript17Nov2017.py
+++ CE/RIPSMasterScript17Nov2017.py
@@ -579,7 +579,7 @@ def callCapacityExpansion(genFleetForCE, hourlyCapacsCE, hourlyCurtailedTechCapa
 
     path_gams = gamsFileDir
     str_target = 'SET z  zones'
-    fname = os.path.join(path_gams, '{}.lst'.format(db.name))
+    fname = os.path.join(path_gams, '_gams_py_gjo0.lst')
 
     with open(fname) as f:
         lines = [line.rstrip('\n') for line in f]
