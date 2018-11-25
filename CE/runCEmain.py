487e1172ae7262f6ef647851209725603267d796
kikoralston <fralston@andrew.cmu.edu>
2018-11-25 13:01:34 -0500

    Changed runCEmain.py in order to read csv file with cases

--- CE/runCEmain.py
+++ CE/runCEmain.py
@@ -1,19 +1,39 @@
+import os
+import pandas as pd
 from RIPSMasterScript17Nov2017 import *
 from Parameters import *
 
-print('Loading parameters and setting up initial data')
-# Load parameters
-genparam = Generalparameters.Generalparameters()
-genparam.load(fname='./generalparameters.txt')
 
-reserveparam = Reserveparameters.Reserveparameters()
-reserveparam.load(fname='./reserveparameters.txt')
+def runCEmain(case=None):
 
-curtailparam = Curtailmentparameters.Curtailmentparameters()
-curtailparam.load(fname='./curtailmentparameters.txt')
+    print('Loading parameters and setting up initial data')
+    # Load parameters
+    genparam = Generalparameters.Generalparameters()
+    genparam.load(fname='./generalparameters.txt')
 
-print()
-print('Parameters loaded! Calling CE masterFunction...')
-print()
-print()
-masterFunction(genparam, reserveparam, curtailparam)
+    reserveparam = Reserveparameters.Reserveparameters()
+    reserveparam.load(fname='./reserveparameters.txt')
+
+    curtailparam = Curtailmentparameters.Curtailmentparameters()
+    curtailparam.load(fname='./curtailmentparameters.txt')
+
+    if case is not None:
+
+        case = int(case) - 1
+
+        df = pd.read_csv('../../scripts/list_cases.csv')
+
+        genparam.resultsDir = df['resultsDir'].iloc(case)
+        curtailparam.listgcms = df['listgcms'].iloc(case)
+
+        print()
+        print('------------------------------------------')
+        print('folder out: {}'.format(genparam.resultsDir))
+        print('list gcms: {}'.format(curtailparam.listgcms))
+        print('------------------------------------------')
+
+    print()
+    print('Parameters loaded! Calling CE masterFunction...')
+    print()
+    print()
+    masterFunction(genparam, reserveparam, curtailparam)
